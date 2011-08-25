e5a6502031489c51595405c9ebfdfd1e8e47ccf5
Mark Cowley <m.cowley0@gmail.com>
2011-08-25 01:24:39 +0000

    added verbose argument handles URL's

--- R/import.gsea.chip.R
+++ R/import.gsea.chip.R
@@ -16,21 +16,24 @@
 #' @param x the path or url to a chip file, or the path to a gsea directory
 #' @param na.strings a character vector of values which should be mapped to NA. see details
 #' @param capitalise.symbol logical: capitalise all gene symbols upon import? see details
+#' @param verbose logical: if TRUE, then write out verbose messages, including download.file
+#'    verbose output.
 #' @author Mark Cowley, 2009-08-07
 #' @export
-import.gsea.chip <- function(x, na.strings=c("---", "NA", ""), capitalise.symbol=TRUE ) {
+import.gsea.chip <- function(x, na.strings=c("---", "NA", ""), capitalise.symbol=TRUE, verbose=TRUE ) {
 	if( is.gsea.dir(x) ) {
 		rpt <- import.gsea.rpt(x)
 		import.gsea.chip( rpt$chip, na.strings=na.strings, capitalise.symbol=capitalise.symbol )
 	}
 	else if( is.url(x) ) {
 		f <- tempfile()
-		cat("Attempting to download .chip file.\n")
-		download.file(x, f)
+		if( verbose ) cat("Downloading .chip file.\n")
+		success <- download.file(x, f, quiet=!verbose)
+		success == 0 || stop(sprintf("Could not download chip file at: '%d'", x))
 		import.gsea.chip(f, na.strings=na.strings, capitalise.symbol=capitalise.symbol)
 	}
 	else if( file.exists(x) ) {
-		chip <- read.delim(x)
+		chip <- read.delim(x, stringsAsFactors=FALSE)
 		for(na in na.strings)
 			chip[chip == na] <- NA
 		chip <- chip[, 1:match("Gene.Title", colnames(chip))]
@@ -39,13 +42,13 @@ import.gsea.chip <- function(x, na.strings=c("---", "NA", ""), capitalise.symbol
 		}
 			
 		if( is.numeric(chip$Probe.Set.ID) ) {
-			message("Probe ID's are numeric - converting them to text.\n")
+			# if( verbose ) message("Probe ID's are numeric - converting them to text.\n")
 			chip$Probe.Set.ID <- as.character(chip$Probe.Set.ID)
 		}
 
 		if( any(nchar(chip$Gene.Symbol)>255) ) {
 			idx <- which(nchar(chip$Gene.Symbol)>255)
-			tmp <- str.left(chip$Gene.Symbol[idx], 255)
+			tmp <- substring(chip$Gene.Symbol[idx], 1, 255)
 			# warning: this will frequently truncate the last gene symbol in the string.
 			# todo: cleanup tmp by nibbling from the end to ensure the strings end on a proper gene symbol
 			chip$Gene.Symbol[idx] <- tmp
