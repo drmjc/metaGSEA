79c948923e8a4d8fc5663e345e822c08d17692af
Mark Cowley <m.cowley0@gmail.com>
2011-08-25 01:26:00 +0000

    + roxygen doc

--- R/is.gsea.dir.R
+++ R/is.gsea.dir.R
@@ -1,3 +1,9 @@
+#' is the argument a valid GSEA directory?
+#'
+#' @param x path to a directory
+#' @return \code{TRUE} if \code{x} is the top-level GSEA result directory, \code{FALSE} otherwise
+#' @author Mark Cowley, 2011-08-04
+#' @export
 is.gsea.dir <- function(x) {
 	!is.null(x) & !is.na(x) & file.exists(x) & is.dir(x) & file.exists(file.path(x, "index.html"))
 }
