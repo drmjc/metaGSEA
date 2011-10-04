d282ee1798875ad8c06e85cb970587345b682536
Mark Cowley <m.cowley0@gmail.com>
2011-10-04 05:54:00 +0000

    improved roxygen

--- R/export.gsea.rnk.R
+++ R/export.gsea.rnk.R
@@ -1,33 +1,18 @@
-#
-# Export a vector of values as a GSEA pre-ranked list.
-#
-# Parameters:
-#	values: a numeric vector
-#	names: the names associated with each value. If NULL, then the names(values) are used
-#	file: the output file name
-#	resort: re-sort the values such that they are decreasing. see order(, decreasing=TRUE)
-#	sigfigs: the number of decimal places, defaults to 4.
-#
-# Value:
-#	<none>, makes a file
-#
-# Mark Cowley, 2008-09-08
-#
-
-
-##' Export a vector of values as a GSEA pre-ranked list.
-##' 
-##' @param values a numeric vector
-##' @param names the names associated with each value. If NULL, then the
-##'   names(values) are used
-##' @param file the output file name
-##' @param resort re-sort the values such that they are decreasing. see order(,
-##'   decreasing=TRUE)
-##' @param sigfigs the number of decimal places, defaults to 4.
-##' @return <none>, makes a file
-##' @author Mark Cowley, 2008-09-08
-##' @export
+#' Export a vector of values as a GSEA pre-ranked list.
+#' 
+#' @param values a numeric vector
+#' @param names the names associated with each value. If \code{NULL}, then the
+#'   \code{names(values)} are used
+#' @param file the output file name. No hyphens, or a warning will be issued.
+#' @param resort logical: re-sort the values such that they are decreasing? see order(,
+#'   decreasing=TRUE)
+#' @param sigfigs the number of decimal places, defaults to 4.
+#' @return <none>, makes a file
+#' @author Mark Cowley, 2008-09-08
+#' @export
 export.gsea.rnk <- function(values, names=NULL, file=NULL, resort=TRUE, sigfigs=4) {
+	if( grepl("-", file) ) warning("file contains hyphens, which GSEA will probably complain about.")
+
 	dir <- dirname(file)
 	if( !file.exists(dir) ) {
 		warning("Creating directory:", dir)
