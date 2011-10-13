879e7e85cb3484aed1b8ed28b91d124e67773b34
Mark Cowley <m.cowley0@gmail.com>
2011-10-13 03:35:43 +0000

    converted to S4

--- R/jacquard.R
+++ R/jacquard.R
@@ -1,35 +1,58 @@
-#
-# The Jacquard coefficient as a measure of overlap in terms of genes in 2 gene sets.
-# Motivated by the leading edge analysis of Broad's GSEA.
-# 
-# Parameters:
-# 	a,b: a vector of entities, be they characters or integers
-#
-# Value:
-# 	a decimal value in [0,1] where 0 represents no overlap, and 1.0 indicating 100% overlap - ie a=b
-#
-# Examples:
-#	jacquard(letters[1:5], letters[3:5])
-# 	# [1] 0.6
-#
-# Mark Cowley, 2009-04-06
-# 
+#' The Jacquard coefficient as a measure of overlap in terms of genes in 2
+#' gene sets.
+#' 
+#' Motivated by the leading edge analysis of Broad's GSEA.
+#' 
+#' @param a a vector of entities, be they \code{character}s, \code{integer}s or \code{GeneSet}
+#' @param b a vector of entities, be they \code{character}s, \code{integer}s or \code{GeneSet}
+#' @return a decimal value in [0,1] where 0 represents no overlap, and 1.0
+#'   indicating 100% overlap - ie a=b
+#' @author Mark Cowley, 2009-04-06
+#' @exportMethod jacquard
+#' @rdname jacquard-methods
+#' @docType methods
+#' @examples
+#' jacquard(letters[1:5], letters[3:5])
+#' jacquard(1:5, 3:5)
+setGeneric(
+	"jacquard",
+	function(a, b) {
+		standardGeneric("jacquard")
+	}
+)
 
 
-##' The Jacquard coefficient as a measure of overlap in terms of genes in 2
-##' gene sets.
-##' 
-##' Motivated by the leading edge analysis of Broad's GSEA.
-##' 
-##' @param a a vector of entities, be they characters or integers
-##' @param b a vector of entities, be they characters or integers
-##' @return a decimal value in [0,1] where 0 represents no overlap, and 1.0
-##'   indicating 100% overlap - ie a=b
-##' @author Mark Cowley, 2009-04-06
-##' @examples
-##' jacquard(letters[1:5], letters[3:5])
-##' # [1] 0.6
-##' @export
-jacquard <- function(a,b) {
-	length(intersect(a,b)) / length(union(a,b))
-}
+#' @rdname jacquard-methods
+#' @aliases jacquard,character,character-method
+setMethod(
+	"jacquard",
+	signature=signature("character", "character"),
+	function(a, b) {
+		length(intersect(a, b)) / length(union(a, b))
+	}
+)
+
+#' @rdname jacquard-methods
+#' @aliases jacquard,numeric,numeric-method
+setMethod(
+	"jacquard",
+	signature=signature("numeric", "numeric"),
+	function(a, b) {
+		length(intersect(a, b)) / length(union(a, b))
+	}
+)
+
+#' @rdname jacquard-methods
+#' @aliases jacquard,GeneSet,GeneSet-method
+setMethod(
+	"jacquard",
+	signature=signature("GeneSet", "GeneSet"),
+	function(a, b) {
+		length(intersect(geneIds(a), geneIds(b))) / length(union(geneIds(a), geneIds(b)))
+	}
+)
+
+# non-S4 method
+# jacquard <- function(a,b) {
+# 	length(intersect(a,b)) / length(union(a,b))
+# }
