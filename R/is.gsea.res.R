1a732ea8f3343e5c2ad97ececd704429b19ca5b4
Mark Cowley <m.cowley0@gmail.com>
2011-10-25 09:10:24 +0000

    + @export

--- R/is.gsea.res.R
+++ R/is.gsea.res.R
@@ -1,6 +1,19 @@
+#' is.gsea.gct.file
+#'
+#' @param f the path to a file
+#' @return \code{TRUE} if \code{f} is a GCT file, \code{FALSE} otherwise
+#' @author Mark Cowley, 2011-10-21
+#' @export
 is.gsea.gct.file <- function(f) {
 	grepl("gct$", f, ignore.case=TRUE)
 }
+
+#' is.gsea.res.file
+#'
+#' @param f the path to a file
+#' @return \code{TRUE} if \code{f} is a RES file, \code{FALSE} otherwise
+#' @author Mark Cowley, 2011-10-21
+#' @export
 is.gsea.res.file <- function(f) {
 	grepl("res$", f, ignore.case=TRUE)
 }
