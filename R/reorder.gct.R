a700c9c6458b04e3bb6691390873b08d1ce7a9f1
Mark Cowley <m.cowley0@gmail.com>
2011-12-01 02:56:25 +0000

    roxygenized added max argument

--- R/reorder.gct.R
+++ R/reorder.gct.R
@@ -1,36 +1,21 @@
-# Function to reorder the rows of a GCT file
-#
-# Details:
-#	take the data in a gct file & reorder the rows by different criteria:
-#	- var: sort by variance, from high to low
-#	- mean: sort by average abundance, from high to low
-#	- median: sort by median abundance, from high to low
-#	- sum: sort by sum, from high to low (useful if the GCT is an adjacency matrix/matrix of counts, or scores)
-#
-# Parameters:
-#	gct: a gct object
-#	method: see details. Defualt = 'var'
-#	reverse: if TRUE, the reverse the default sort order (see details for default sort order)
-#
-# Mark Cowley, 2011-03-16
-
-
-##' Function to reorder the rows of a GCT file
-##' 
-##' take the data in a gct file & reorder the rows by different criteria:
-##' - var: sort by variance, from high to low
-##' - mean: sort by average abundance, from high to low
-##' - median: sort by median abundance, from high to low
-##' - sum: sort by sum, from high to low (useful if the GCT is an adjacency
-##' matrix/matrix of counts, or scores)
-##' 
-##' @param gct a gct object
-##' @param method see details. Defualt = 'var'
-##' @param reverse if TRUE, the reverse the default sort order (see details for
-##'   default sort order)
-##' @author Mark Cowley, 2011-03-16
-##' @export
-reorder.gct <- function(gct, method=c("var", "mean", "median", "sum"), reverse=FALSE) {
+#' Function to reorder the rows of a GCT object
+#' 
+#' take the data in a gct file & reorder the rows by different criteria:\cr
+#' - var: sort by variance, from high to low\cr
+#' - mean: sort by average abundance, from high to low\cr
+#' - median: sort by median abundance, from high to low\cr
+#' - max: sort by max abundance, from high to low\cr
+#' - sum: sort by sum, from high to low (useful if the GCT is an adjacency
+#' matrix/matrix of counts, or scores)\cr
+#' 
+#' @param gct a gct object
+#' @param method see details. Default = 'var'
+#' @param reverse if \code{TRUE}, then reverse the default sort order (see details for
+#'   default sort order)
+#' @author Mark Cowley, 2011-03-16
+#' @export
+#' @seealso \code{\link{reorder.gct.file}}
+reorder.gct <- function(gct, method=c("var", "mean", "median", "max", "sum"), reverse=FALSE) {
 	N <- ncol(gct)-2
 	
 	method <- method[1]
@@ -51,39 +36,29 @@ reorder.gct <- function(gct, method=c("var", "mean", "median", "sum"), reverse=F
 			x <- apply(gct[,3:(N+2)], 1, sum)
 			gct[order(x, decreasing=TRUE), ]
 		},
+		max={
+			x <- apply(gct[,3:(N+2)], 1, max)
+			gct[order(x, decreasing=TRUE), ]
+		},
 		stop("Unsupported method")
 	)
 	
 	return(gct)
 }
 
-# Reorder the rows of a GCT file.
-#
-# Details: see reorder.gct
-#
-# Parameters:
-#	gct.file, method, reverse: see reorder.gct
-#	gct.file.out: the name of the GCT file to be created with the reordered rows
-#
-# Value:
-#	creates a file at gct.file.out, where the rows have been reordered
-#
-# Mark Cowley, 2011-03-16
-
-
-##' Reorder the rows of a GCT file.
-##' 
-##' Details: see reorder.gct
-##' 
-##' @param gct.file see reorder.gct
-##' @param method see reorder.gct
-##' @param reverse see reorder.gct
-##' @param gct.file.out the name of the GCT file to be created with the
-##'   reordered rows
-##' @return creates a file at gct.file.out, where the rows have been reordered
-##' @author Mark Cowley, 2011-03-16
-##' @export
-reorder.gct.file <- function(gct.file, gct.file.out, method=c("var", "mean", "median", "sum"), reverse=FALSE) {
+#' Reorder the rows of a GCT file.
+#' 
+#' see \code{\link{reorder.gct}} for more details
+#' 
+#' @param gct.file the path to a gct file
+#' @param gct.file.out the name of the GCT file to be created with the
+#'   reordered rows
+#' @inheritParams reorder.gct
+#' @return creates a file at gct.file.out, where the rows have been reordered
+#' @author Mark Cowley, 2011-03-16
+#' @export
+#' @seealso \code{\link{reorder.gct}}
+reorder.gct.file <- function(gct.file, gct.file.out, method=c("var", "mean", "median", "max", "sum"), reverse=FALSE) {
 	gct <- import.gsea.gct(gct.file)
 	gct <- reorder.gct(gct, method=method, reverse=reverse)
 	export.gsea.gct(gct, file=gct.file.out)
