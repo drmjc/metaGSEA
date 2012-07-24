effab9fd4bf79b7a5a52d60e611dcefc33cfb271
Mark Cowley <m.cowley0@gmail.com>
2012-07-24 07:34:49 +0000

    added XML imports

--- R/export.gsea.edb.R
+++ R/export.gsea.edb.R
@@ -8,6 +8,7 @@
 #' @seealso \code{\link{import.gsea.edb}}, \code{\link[XML]{saveXML}}
 #' @keywords file IO
 #' @export
+#' @importFrom XML saveXML
 export.gsea.edb <- function(edb, file) {
 	saveXML(edb, file, encoding="UTF-8")
 	perl.oneliner("perl -pi -e 's|<DTG|\n  <DTG|g'", file)
